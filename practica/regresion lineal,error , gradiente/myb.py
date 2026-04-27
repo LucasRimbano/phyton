a5da322dcf1c46de6dc8e4bff77e684850432a7f
Lucas Rimbano <Lucasrimbano@gmail.com>
2026-04-27 12:17:20 -0300

    Agregando unidad 5 redes neurnales  y un ejercicio de regresion lineal

--- practica/regresion lineal,error , gradiente/myb.py
+++ practica/regresion lineal,error , gradiente/myb.py
@@ -2,10 +2,15 @@
       #  a las variables m y b.
 
 from sympy import symbols, Function, Sum, diff
-m ,b ,i , n = symbols('m b i n')
-x,y = symbols('x y', cls=Function)
-sum_of_squares = sum((m*b(i) +b - y(i))**2,(i,o,n) )
-d_m = diff(sum_of_squares ,m)
-d_b = diff (sum_of_squares, b)
+
+m, b, i, n = symbols('m b i n')
+x, y = symbols('x y', cls=Function)
+
+sum_of_squares = Sum((m*x(i) + b - y(i))**2, (i, 0, n))
+
+d_m = diff(sum_of_squares, m)
+d_b = diff(sum_of_squares, b)
+
+print("Error cuadrático:", sum_of_squares)
 print("Derivada parcial respecto a m:", d_m)
 print("Derivada parcial respecto a b:", d_b)
